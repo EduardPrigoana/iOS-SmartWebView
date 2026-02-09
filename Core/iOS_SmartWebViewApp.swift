37bffb5a20f928d3964b7df58a1c62324e5ccd6e
Eduard Prigoana <eduard@prigoana.com>
2026-02-09 12:12:05 +0000

    Cleanup: Remove unused PluginManager and PermissionManager
    - Deleted Managers/PluginManager.swift (no plugins in use)
    - Deleted Managers/PermissionManager.swift (no permissions requested)
    - Removed permission initialization from app startup
    - Updated project.pbxproj to remove all references
    - Project is now minimal WebView wrapper with popup support

--- Core/iOS_SmartWebViewApp.swift
+++ Core/iOS_SmartWebViewApp.swift
@@ -6,10 +6,6 @@ struct iOS_SmartWebViewApp: App {
     // This connects our AppDelegate to the SwiftUI app lifecycle.
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
-    init() {
-        PermissionManager.shared.requestInitialPermissions()
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
